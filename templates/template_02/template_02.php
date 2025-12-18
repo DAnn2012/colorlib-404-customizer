c9c3280946d9a9e0f73d6ca4b2f6add2cf00d02a
DAnn2012 <dannunziata@hotmail.it>
2025-12-18 13:07:57 +0100

    Update template_02.php

--- templates/template_02/template_02.php
+++ templates/template_02/template_02.php
@@ -7,7 +7,7 @@
         <h2 id="colorlib_404_customizer_page_heading"><?php echo wp_kses_post( $cnfp_options['colorlib_404_customizer_page_heading'] ); ?></h2>
         <form class="colorlib-notfound-search" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search"
               method="get">
-            <input type="search" name="s" placeholder="<?php esc_attr__( 'Search...', 'colorlib-404-customizer' ); ?>">
+            <input type="search" name="s" placeholder="<?php esc_attr_e( 'Search...', 'colorlib-404-customizer' ); ?>">
             <button type="submit"><span></span></button>
         </form>
     </div>
